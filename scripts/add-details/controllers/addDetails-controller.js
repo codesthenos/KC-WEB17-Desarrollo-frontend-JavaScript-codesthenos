1a5acdc84cdadba8242accbb6f3ca1b4c95ae1f3
codesthenos <codesthenos@gmail.com>
2024-11-17 19:00:40 +0100

    Comento el timeout, pero no lo borro, ya que creo que cuando implemente el resto de funcionalidad a la web quiza haga algo asincrono con la base de datos como borrar o updatear, por lo que quiza si que necesite el loading si hago algo asincrono

--- scripts/add-details/controllers/addDetails-controller.js
+++ scripts/add-details/controllers/addDetails-controller.js
@@ -1,9 +1,16 @@
+// import { loadingController } from '../../loading/loading-controller.js'
 import { addDetailsModel } from '../models/addDetails-model.js'
-import { loadingController } from '../../loading/loading-controller.js'
 import { errorController } from '../../error/error-controller.js'
 import { addController } from './add-controller.js'
 
 export const addDetailsController = () => {
+  const data = addDetailsModel()
+  if (data.error) {
+    errorController({ errorMessage: data.error })
+  } else if (data.add) {
+    addController({ add: data.add })
+  }
+  /* This was to test the reusability of the loading, but this web doesnt fetch yet anything but will do probably
   loadingController()
   setTimeout(() => {
     const data = addDetailsModel()
@@ -13,4 +20,5 @@ export const addDetailsController = () => {
       addController({ add: data.add })
     }
   }, 1000)
+  */
 }
