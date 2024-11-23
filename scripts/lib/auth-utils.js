6be7307349df1bda4ed95baf75815446d70cac2c
codesthenos <codesthenos@gmail.com>
2024-11-23 16:55:03 +0100

    Al final decido sacar la funcion al archivo auth-utils.js que ahora ademas de tener la funcion de validar, que ahora la modificare para que sean dos funciones distintas, tambien he metido ahi dos unciones, una para obetener los datos del login que tengo que hacer, y otra para obtener los datos del register (del DOM me refiero) y ahora aunque hay que venir a este doc para entender la logica bien, por el nombre de las funciones creo que se enteinde bien que hace el controller y es muy cortito para todos los estados que maneja

--- scripts/lib/auth-utils.js
+++ scripts/lib/auth-utils.js
@@ -1,4 +1,6 @@
-import { REGEXP } from './consts.js'
+import { errorNoti, REGEXP, SUCCESS_MESSAGES, successNoti } from './consts.js'
+import { fireNotificationEvent } from './fire-notification-event.js'
+import { registerUser } from '../auth-models/register-model.js'
 
 export const validateRegisterLogin = ({ userEmail, userPassword, userPasswordConfirm }) => {
   const errors = []
@@ -12,4 +14,36 @@ export const validateRegisterLogin = ({ userEmail, userPassword, userPasswordCon
     errors.push('Passwords doesn\'t match')
   }
   return errors
+}
+
+export const takeLoginInputsValue = ({ emailId, passId }) => {
+  const userEmailInput = document.getElementById(emailId)
+  const userPasswordInput = document.getElementById(passId)
+
+  const userEmail = userEmailInput.value
+  const userPassword = userPasswordInput.value
+
+  return { userEmail, userPassword }
+}
+
+export const takeRegisterInputsValue = ({ emailId, passId, passConfirmId }) => {
+  const userPasswordConfirmInput = document.getElementById(passConfirmId)
+  
+  const { userEmail, userPassword } = takeLoginInputsValue({ emailId, passId })
+
+  const userPasswordConfirm = userPasswordConfirmInput.value
+  
+  return { userEmail, userPassword, userPasswordConfirm }
+}
+
+export const handleRegister = async ({ element, userEmail, userPassword }) => {
+  try {
+    await registerUser({ userEmail, userPassword })
+    fireNotificationEvent({ element, type: successNoti, message: SUCCESS_MESSAGES.REGISTERED })
+    setTimeout(() => {
+      window.location.href = '/'
+    }, 1500)
+  } catch (error) {
+    fireNotificationEvent({ element, type: errorNoti, errorList: [error.message] })
+  }
 }
--- scripts/register/register-controller.js
+++ scripts/register/register-controller.js
@@ -1,7 +1,6 @@
+import { errorNoti, loadingNoti } from '../lib/consts.js'
 import { fireNotificationEvent } from '../lib/fire-notification-event.js'
-import { errorNoti, loadingNoti, SUCCESS_MESSAGES, successNoti } from '../lib/consts.js'
-import { validateRegisterLogin } from '../lib/auth-utils.js'
-import { registerUser } from '../auth-models/register-model.js'
+import { handleRegister, takeRegisterInputsValue, validateRegisterLogin } from '../lib/auth-utils.js'
 
 export const registerController = ({ element }) => {
   element.addEventListener('submit', (event) => {
@@ -9,13 +8,15 @@ export const registerController = ({ element }) => {
 
     fireNotificationEvent({ element, type: loadingNoti })
     
-    const userEmailInput = document.getElementById('email')
-    const userPasswordInput = document.getElementById('password')
-    const userPasswordConfirmInput = document.getElementById('password-confirm')
-    
-    const userEmail = userEmailInput.value
-    const userPassword = userPasswordInput.value
-    const userPasswordConfirm = userPasswordConfirmInput.value
+    const {
+      userEmail,
+      userPassword,
+      userPasswordConfirm
+    } = takeRegisterInputsValue({
+      emailId: 'email',
+      passId: 'password',
+      passConfirmId: 'password-confirm'
+    })
     
     const errors = validateRegisterLogin({ userEmail, userPassword, userPasswordConfirm })
     
@@ -25,16 +26,4 @@ export const registerController = ({ element }) => {
       handleRegister({ element, userEmail, userPassword })
     }
   })
-}
-
-const handleRegister = async ({ element, userEmail, userPassword }) => {
-  try {
-    await registerUser({ userEmail, userPassword })
-    fireNotificationEvent({ element, type: successNoti, message: SUCCESS_MESSAGES.REGISTERED })
-    setTimeout(() => {
-      window.location.href = '/'
-    }, 1500)
-  } catch (error) {
-    fireNotificationEvent({ element, type: errorNoti, errorList: [error.message] })
-  }
 }
